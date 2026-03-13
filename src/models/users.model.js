7f296d2cb9b3064d01608d77fbdacd0d8eefe6ad
B-Payal <b08.payal@gmail.com>
2026-03-13 09:43:02 +0530

    mongoose middleware added

--- src/models/users.model.js
+++ src/models/users.model.js
@@ -1,4 +1,5 @@
 const mongoose  = require("mongoose")
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
     username:{
@@ -13,8 +14,23 @@ const userSchema = new mongoose.Schema({
         unique:true,
         trim:true,
         lowercase:true
+    },
+    password:{
+        type:String,
+        required:true
     }
-} , {timestamps:true})
+} , {timestamps:true});
+
+userSchema.pre('save' , async function(next){
+    if(!this.isModified('password')){
+        return next();
+    }
+    try{
+        this.password = await bcrypt.hash(this.password , 10);
+    }catch(err){
+        next(err);
+    }
+})
 
 const User = mongoose.model('User' , userSchema)
 
