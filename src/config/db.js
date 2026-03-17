f5f7640bf544aeb1c52d132f412c925330674ca8
B-Payal <b08.payal@gmail.com>
2026-03-17 14:20:45 +0530

    changed a file

--- src/config/db.js
+++ src/config/db.js
@@ -1,5 +1,5 @@
 const mongoose  = require("mongoose")
-const connect = async ()=>{
+exports.connect = async ()=>{
     try{
     await mongoose.connect(process.env.MONGO_URI);
     console.log('connected to mongoDB')}catch(error){
@@ -8,4 +8,3 @@ const connect = async ()=>{
 }
 
 
-module.exports = connect;
